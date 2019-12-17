7de38d61c625ed91e14443090428ac9e929841a6
Nelson <tomneo2004@gmail.com>
2019-12-17 17:54:40 +0800

    test route test json response

--- routes/testRoute.js
+++ routes/testRoute.js
@@ -23,6 +23,14 @@ router.get('/testFail', asyncHandler(async (req, res, next)=>{
     throwFail(testFailTypes.FATAL_FAIL);
 }));
 
+router.post('/testJson', asyncHandler(async (req, res, next)=>{
+
+    const data = {message:"this is from server", math:1576575267643, msg:"dd", data:{d1:"ddvd", d2:"fff"}};
+    // res.status(200).json(data);
+    const data2 = {"code": 500,"message": "Database error"}
+    res.success(data2);
+}));
+
 //test api key
 router.post('/testAPIKey', asyncHandler(apiKeyValidator), asyncHandler(async (req, res, next)=>{
 
