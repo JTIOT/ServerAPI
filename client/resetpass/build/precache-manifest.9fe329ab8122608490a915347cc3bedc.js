16139c9c375685e8caa08156d068711b03667239
Nelson <tomneo2004@gmail.com>
2020-01-15 12:12:11 +0800

    before merge from dev

---
+++ client/resetpass/build/precache-manifest.9fe329ab8122608490a915347cc3bedc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fabaa32588d534336a1ddc9a5c2f3f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1868bdac63459ba7bb92",
+    "url": "/static/css/main.1beb607f.chunk.css"
+  },
+  {
+    "revision": "ca2c1f2e03c97047e3b4",
+    "url": "/static/js/2.df8bfc91.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.df8bfc91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1868bdac63459ba7bb92",
+    "url": "/static/js/main.ad28d8ef.chunk.js"
+  },
+  {
+    "revision": "2af4c5148963bbfa57a1",
+    "url": "/static/js/runtime-main.e68eef39.js"
+  }
+]);
