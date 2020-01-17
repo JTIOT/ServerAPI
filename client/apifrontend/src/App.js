772cf957dcadc1297980232ae0a03308365ee97a
Nelson <tomneo2004@gmail.com>
2020-01-17 08:54:23 +0800

    frontend build

--- client/apifrontend/src/App.js
+++ client/apifrontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import QRCodeReader from './components/qr-reader/qr-reader';
+import ResetPassword from './components/resetPassword/resetPassword';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <QRCodeReader />
+      <Switch>
+        <Route exact path='/qrcode-scanner/' component={QRCodeReader} />
+        <Route exact path='/resetPassword/:userId/:token' component={ResetPassword} />
+        <Route path='*' render={()=><div>Page not found</div>} />
+      </Switch>    
     </div>
   );
 }
